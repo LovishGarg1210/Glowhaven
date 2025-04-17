837c6b661483d740e6b7720d65309378a16315a4
LovishGarg1210 <lgarg1210@gmail.com>
2025-04-17 16:40:05 +0530

    img chnge

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,16 +3,12 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-         <div
-      style={{
-        backgroundImage: "url('./hero-about.webp')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '90vh',
-      }}
-      className="flex items-center justify-center px-6 md:px-10"
-    ></div>
-      
+        <div
+  className="flex items-center justify-center px-6 md:px-10 h-[90vh] bg-[url('/hero-about-mob.webp')] md:bg-[url('/Aboutt.webp')] bg-cover bg-center"
+>
+  {/* Your content here */}
+</div>
+
     </div>
   )
 }
